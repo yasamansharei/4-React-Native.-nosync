c8ee318e4a372bacb5382a65a640c96a339cd24d
ydianatpey <ydianatpey@gmail.com>
2023-10-27 19:01:01 -0700

    Icons, Favorites, and Comments

--- nucampsite/screens/CampsiteInfoScreen.js
+++ nucampsite/screens/CampsiteInfoScreen.js
@@ -1,8 +1,15 @@
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { COMMENTS } from 'm../shared/comments'
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    return <RenderCampsite campsite={campsite} />;
+    const [comments, setComments] = useState(COMMENTS);
+    return (
+
+        
+    )
 };
 
 export default CampsiteInfoScreen;
